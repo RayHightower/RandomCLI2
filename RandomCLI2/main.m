2f381b4812b2352a50a42158319db81fb9e92619
RayHightower <rth@wisdomgroup.com>
2012-04-16 16:16:37 -0500

    Initial Commit

---
+++ RandomCLI2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomCLI2
+//
+//  Created by Raymond T. Hightower on 4/16/12.
+//  Copyright (c) 2012 WisdomGroup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
